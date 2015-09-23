97ab6329a17e2f61e38d80bbefe3fba6ed7d9c5a
dgomesbr <dgomesbr@gmail.com>
2015-09-22 23:34:13 -0300

    fix for spring-guides/gs-consuming-rest#13

--- complete/src/main/java/hello/Quote.java
+++ complete/src/main/java/hello/Quote.java
@@ -1,13 +1,52 @@
 package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
 
     private String type;
     private Value value;
 
+    public Quote() {
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Quote)) return false;
+
+        Quote quote = (Quote) o;
+
+        if (type != null ? !type.equals(quote.type) : quote.type != null) return false;
+        return !(value != null ? !value.equals(quote.value) : quote.value != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type != null ? type.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return "hello.Quote(type=" + this.type + ", value=" + this.value + ")";
+    }
 }
--- complete/src/main/java/hello/Value.java
+++ complete/src/main/java/hello/Value.java
@@ -1,13 +1,52 @@
 package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Value {
 
     private Long id;
     private String quote;
 
+    public Value() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getQuote() {
+        return this.quote;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Value)) return false;
+
+        Value value = (Value) o;
+
+        if (id != null ? !id.equals(value.id) : value.id != null) return false;
+        return !(quote != null ? !quote.equals(value.quote) : value.quote != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (quote != null ? quote.hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return "hello.Value(id=" + this.id + ", quote=" + this.quote + ")";
+    }
 }
